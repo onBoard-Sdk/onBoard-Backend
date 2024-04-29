5be49b9c904821845236fa03bee206227cbc062b
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 00:56:00 +0900

    feat: exception filter 등록

---
+++ src/main/kotlin/com/onboard/server/global/filter/GlobalExceptionFilter.kt
@@ -0,0 +1,36 @@
+package com.onboard.server.global.filter
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.onboard.server.global.error.BusinessException
+import com.onboard.server.global.error.ErrorResponse
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.web.filter.OncePerRequestFilter
+import java.nio.charset.StandardCharsets
+
+class GlobalExceptionFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: BusinessException) {
+            setErrorResponse(e.status, e.message, response)
+        }
+    }
+
+    private fun setErrorResponse(status: Int, message: String, response: HttpServletResponse) {
+        response.apply {
+            this.status = status
+            this.contentType = MediaType.APPLICATION_JSON_VALUE
+            this.characterEncoding = StandardCharsets.UTF_8.name()
+            this.writer.write(objectMapper.writeValueAsString(ErrorResponse.of(status, message)))
+        }
+    }
+}
--- src/main/kotlin/com/onboard/server/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/onboard/server/global/security/SecurityConfig.kt
@@ -1,5 +1,9 @@
 package com.onboard.server.global.security
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.onboard.server.global.filter.GlobalExceptionFilter
+import com.onboard.server.global.filter.JwtFilter
+import com.onboard.server.global.security.jwt.JwtParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -9,9 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtParser: JwtParser,
+    private val objectMapper: ObjectMapper,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
@@ -25,6 +33,8 @@ class SecurityConfig {
                     .requestMatchers(HttpMethod.GET, "api/v1/auth/codes").permitAll()
                     .anyRequest().authenticated()
             }
+            .addFilterBefore(JwtFilter(jwtParser), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(GlobalExceptionFilter(objectMapper), JwtFilter::class.java)
             .build()
     }
 
