5a9323df5336f655622643302af7379ce70ee702
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 00:18:53 +0900

    feat: RedisConfig

---
+++ src/main/kotlin/com/onboard/server/global/config/redis/RedisConfig.kt
@@ -0,0 +1,26 @@
+package com.onboard.server.global.config.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfiguration = RedisStandaloneConfiguration().apply {
+            hostName = redisProperties.host
+            port = redisProperties.port
+            password = RedisPassword.of(redisProperties.password)
+        }
+
+        return LettuceConnectionFactory(redisConfiguration)
+    }
+}
