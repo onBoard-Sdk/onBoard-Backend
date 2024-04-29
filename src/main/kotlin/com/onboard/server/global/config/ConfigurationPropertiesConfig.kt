cf57c63d826263d46d948c0056aad590d37d5012
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 00:16:30 +0900

    init: ConfigurationPropertiesConfig

---
+++ src/main/kotlin/com/onboard/server/global/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package com.onboard.server.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan("com.onboard.server")
+class ConfigurationPropertiesConfig
