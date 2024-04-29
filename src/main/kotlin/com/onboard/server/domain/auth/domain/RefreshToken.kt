808292fc49996524e82d36cbe72176e8c7a8c695
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 00:55:13 +0900

    feat: RefreshToken

---
+++ src/main/kotlin/com/onboard/server/domain/auth/domain/RefreshToken.kt
@@ -0,0 +1,22 @@
+package com.onboard.server.domain.auth.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
+
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 14)
+class RefreshToken(
+    @Id
+    val accountId: String,
+
+    token: String
+) {
+    @Indexed
+    private var token = token
+
+    val getToken = token
+
+    fun updateToken(token: String) {
+        this.token = token
+    }
+}
---
+++ src/main/kotlin/com/onboard/server/domain/auth/domain/RefreshTokenRepository.kt
@@ -0,0 +1,6 @@
+package com.onboard.server.domain.auth.domain
+
+import org.springframework.data.repository.CrudRepository
+
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
+}
