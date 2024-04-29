cf319a1e8cf66d04cfca91e597d8936562e9528f
alsdl0629 <ehfehf0629@gmail.com>
2024-04-29 20:55:35 +0900

    init: project

---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
+	id("org.asciidoctor.jvm.convert")
+	kotlin("jvm")
+	kotlin("plugin.spring")
+	kotlin("plugin.jpa")
+}
+
+val projectGroup: String by project
+val applicationVersion: String by project
+
+group = projectGroup
+version = applicationVersion
+
+java {
+    sourceCompatibility = JavaVersion.valueOf("VERSION_${property("javaVersion")}")
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+    // kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // presentation
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // persistence
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+    // mail
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+
+    // security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:${property("kotestExtensionsSpringVersion")}")
+    testImplementation("io.kotest:kotest-runner-junit5:${property("kotestVersion")}")
+    testImplementation("io.kotest:kotest-assertions-core:${property("kotestVersion")}")
+    testImplementation("io.mockk:mockk:${property("mockkVersion")}")
+    testImplementation("it.ozimov:embedded-redis:${property("embeddedRedisVersion")}")
+    runtimeOnly("com.h2database:h2")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
+}
+
+tasks.register<Copy>("installGitHooks") {
+    from(file("$rootDir/.githooks"))
+    into(file("$rootDir/.git/hooks"))
+    fileMode = "0775".toInt()
+}
