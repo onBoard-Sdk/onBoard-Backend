e707c7e3675673c4922b2f26411b98f7d578cd58
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 00:23:18 +0900

    chore: JWT 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,11 @@ dependencies {
 
     // presentation
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // persistence
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	runtimeOnly("com.mysql:mysql-connector-j")
 
     // mail
@@ -43,6 +44,11 @@ dependencies {
 
     // security
 	implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 
     // test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
