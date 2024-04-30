19580ed6cc0ff745a2840e3ee3ffa9bb5739f26f
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 09:27:19 +0900

    chore: import 수정

--- src/main/kotlin/com/onboard/server/domain/auth/domain/AuthCodeRepository.kt
+++ src/main/kotlin/com/onboard/server/domain/auth/domain/AuthCodeRepository.kt
@@ -1,6 +1,5 @@
 package com.onboard.server.domain.auth.domain
 
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface AuthCodeRepository : CrudRepository<AuthCode, String> {
--- src/main/kotlin/com/onboard/server/domain/team/domain/Team.kt
+++ src/main/kotlin/com/onboard/server/domain/team/domain/Team.kt
@@ -3,14 +3,17 @@ package com.onboard.server.domain.team.domain
 import com.onboard.server.global.entity.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
 class Team(
     @Id
-    val id: Long,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
 
-    @Column(nullable = false, columnDefinition = "varchar(60)")
+    @Column(nullable = false, unique = true, columnDefinition = "varchar(60)")
     val email: String,
 
     @Column(nullable = false, columnDefinition = "char(60)")
