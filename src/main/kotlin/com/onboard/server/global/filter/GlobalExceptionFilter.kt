f5a10490a41c26aed3734b5073bfda489473d527
alsdl0629 <ehfehf0629@gmail.com>
2024-04-30 09:28:51 +0900

    chore: server error handling

--- src/main/kotlin/com/onboard/server/global/filter/GlobalExceptionFilter.kt
+++ src/main/kotlin/com/onboard/server/global/filter/GlobalExceptionFilter.kt
@@ -3,6 +3,7 @@ package com.onboard.server.global.filter
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.onboard.server.global.error.BusinessException
 import com.onboard.server.global.error.ErrorResponse
+import com.onboard.server.global.exception.InternalServerErrorException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -22,6 +23,12 @@ class GlobalExceptionFilter(
             filterChain.doFilter(request, response)
         } catch (e: BusinessException) {
             setErrorResponse(e.status, e.message, response)
+        } catch (e: Exception) {
+            setErrorResponse(
+                status = InternalServerErrorException.status,
+                message = e.message ?: InternalServerErrorException.message,
+                response = response
+            )
         }
     }
 
