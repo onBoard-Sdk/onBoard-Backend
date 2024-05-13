e8e735a833ab1ea15155083fdb3e1df28da1b22b
alsdl0629 <ehfehf0629@gmail.com>
2024-05-13 19:24:40 +0900

    test: 서비스 삭제

--- src/test/kotlin/com/onboard/server/domain/service/service/ServiceServiceTest.kt
+++ src/test/kotlin/com/onboard/server/domain/service/service/ServiceServiceTest.kt
@@ -13,9 +13,11 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 
 @SpringBootTest
 class ServiceServiceTest : DescribeSpec() {
@@ -132,6 +134,64 @@ class ServiceServiceTest : DescribeSpec() {
                 it("서비스 수정에 실패한다") {
                     shouldThrow<ServiceNotFoundException> {
                         serviceService.modify(subject, 0, request)
+                    }
+                }
+            }
+        }
+
+        this.describe("remove") {
+            context("자신이 만든 서비스이면") {
+                val savedTeam = teamRepository.save(
+                    Team(
+                        email = "email",
+                        password = "password",
+                        name = "name",
+                        logoImageUrl = "logoImageUrl"
+                    )
+                )
+
+                val savedService = serviceRepository.save(
+                    Service(
+                        team = savedTeam,
+                        name = "name",
+                        logoImageUrl = "logoImageUrl",
+                        serviceUrl = "serviceUrl",
+                    )
+                )
+
+                val subject = Subject(savedTeam.id)
+
+                it("서비스를 삭제한다") {
+                    serviceService.remove(subject, savedService.id)
+
+                    serviceRepository.findByIdOrNull(savedService.id).apply {
+                        this shouldBe null
+                    }
+                }
+            }
+
+            context("서비스를 삭제하는 팀의 정보를 찾지 못하면") {
+                it("서비스를 삭제할 수 없다") {
+                    shouldThrow<TeamNotFoundException> {
+                        serviceService.remove(temporarySubject, 1)
+                    }
+                }
+            }
+
+            context("삭제할 서비스를 찾지 못하면") {
+                val savedTeam = teamRepository.save(
+                    Team(
+                        email = "email",
+                        password = "password",
+                        name = "name",
+                        logoImageUrl = "logoImageUrl"
+                    )
+                )
+                val subject = Subject(savedTeam.id)
+
+                it("서비스를 삭제할 수 없다") {
+                    shouldThrow<ServiceNotFoundException> {
+                        serviceService.remove(subject, 1)
                     }
                 }
             }
