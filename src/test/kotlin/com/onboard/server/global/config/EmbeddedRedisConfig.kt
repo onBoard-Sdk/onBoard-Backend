a58180443f647f5b8b4b9277d5c6bb328617ba7f
alsdl0629 <ehfehf0629@gmail.com>
2024-05-14 10:07:19 +0900

    fix: 사용되지 않는 port 찾기

--- src/test/kotlin/com/onboard/server/global/config/EmbeddedRedisConfig.kt
+++ src/test/kotlin/com/onboard/server/global/config/EmbeddedRedisConfig.kt
@@ -5,21 +5,67 @@ import jakarta.annotation.PreDestroy
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.TestConfiguration
 import redis.embedded.RedisServer
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 @TestConfiguration
 class EmbeddedRedisConfig(
     @Value("\${spring.data.redis.port}")
     private val redisPort: Int,
 ) {
-    private val redisServer: RedisServer = RedisServer(redisPort)
+    private lateinit var redisServer: RedisServer
 
     @PostConstruct
     fun startRedis() {
+        val port = if (isRedisRunning()) findAvailablePort() else redisPort
+        redisServer = RedisServer(port)
         redisServer.start()
     }
 
     @PreDestroy
     fun stopRedis() {
         redisServer.stop()
+    }
+
+    private fun isRedisRunning(): Boolean {
+        return isRunning(executeGrepProcessCommand(redisPort))
+    }
+
+    fun findAvailablePort(): Int {
+        for (port in 10000..65535) {
+            val process = executeGrepProcessCommand(port)
+            if (!isRunning(process)) {
+                return port
+            }
+        }
+
+        throw IllegalArgumentException("Not Found Available port: 10000 ~ 65535")
+    }
+
+    private fun executeGrepProcessCommand(port: Int): Process {
+        val command = String.format("netstat -nat | grep LISTEN|grep %d", port)
+        val shell = arrayOf("/bin/sh", "-c", command)
+        return Runtime.getRuntime().exec(shell)
+    }
+
+    private fun isRunning(process: Process): Boolean {
+        var line: String?
+        val pidInfo = StringBuilder()
+
+        try {
+            BufferedReader(InputStreamReader(process.inputStream)).use { input ->
+                while ((input.readLine().also {
+                        line = it
+                    }) != null) {
+                    pidInfo.append(line)
+                }
+            }
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+
+
+        return pidInfo.toString().isNotEmpty()
     }
 }
