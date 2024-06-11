694ad6f8903ef90166acb6d880a98498687ff47d
alsdl0629 <ehfehf0629@gmail.com>
2024-06-11 13:40:21 +0900

    style: 사용하지 않는 import 제거

--- src/main/kotlin/com/onboard/server/global/filter/JwtFilter.kt
+++ src/main/kotlin/com/onboard/server/global/filter/JwtFilter.kt
@@ -1,7 +1,5 @@
 package com.onboard.server.global.filter
 
-import com.onboard.server.global.security.jwt.JwtConstant.HEADER
-import com.onboard.server.global.security.jwt.JwtConstant.PREFIX
 import com.onboard.server.global.security.jwt.JwtParser
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
@@ -10,12 +8,12 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
 class JwtFilter(
-    private val jwtParser: JwtParser
+    private val jwtParser: JwtParser,
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
-        filterChain: FilterChain
+        filterChain: FilterChain,
     ) {
         val token = jwtParser.getToken(request)
         token?.let {
